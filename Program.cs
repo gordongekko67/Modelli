89fd4af478d032a6f0eae2e8a3ecc7c1b2b978cb
gordongekko67 <34415973+gordongekko67@users.noreply.github.com>
2020-02-03 08:08:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String numMacchina = "42303";
+            String codAzienda = "Azienda1";
+            String a = "https://libellula.kepler186.org/remote.php/webdav/";
+            String newWebDav = a + codAzienda + "/"+ numMacchina + "/";
+            Console.WriteLine(newWebDav);
+
+
+
+
+        }
+    }
+}
